086df00f9046805e34300b2d7aefae71483e767a
Stran12345 <tstevent83@gmail.com>
2024-09-01 10:56:52 -0500

    removed un needed comments

--- backend/release.py
+++ backend/release.py
@@ -8,21 +8,18 @@ def get_release_date(song_name, artist_name, entity='song'):
     params = {
         'term': search_term,
         'entity': entity,
-        'limit': 1  # Limit to 1 result for simplicity
+        'limit': 1  
     }
     
     response = requests.get(base_url, params=params)
     data = response.json()
     
     if data['resultCount'] > 0:
-        # Extract release date from the first result
         result = data['results'][0]
         iso_date_str = result.get('releaseDate', None)
         
         if iso_date_str:
-            # Convert the ISO 8601 date string to a datetime object
             date_object = datetime.fromisoformat(iso_date_str.replace('Z', '+00:00'))
-            # Format the datetime object to a string in YYYY-MM-DD format
             formatted_date = date_object.strftime('%Y-%m-%d')
             return formatted_date
         else:
